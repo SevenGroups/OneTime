fec68fe59cff115c7fc9950743cbbe644528dd5d
rxy000 <271682867@qq.com>
2018-04-24 11:46:15 +0800

    11

---
+++ app/src/main/java/com/bw/com/onetimedemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bw.com.onetimedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"一刻钟",Toast.LENGTH_LONG).show();
+    }
+}
