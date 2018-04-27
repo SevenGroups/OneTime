7a2d4fb95c5d04dccba1d51915da4ec68e605443
tangxueqin <haiyan.xu.vip@gmail.com>
2018-04-27 13:51:31 +0800

    1

--- app/src/main/java/com/bw/com/onetimedemo/MyGuanZhuActivity.java
+++ app/src/main/java/com/bw/com/onetimedemo/MyGuanZhuActivity.java
@@ -1,26 +1,51 @@
 package com.bw.com.onetimedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bw.com.onetimedemo.bean.RemMenGuanZhuBean;
 import com.bw.com.onetimedemo.presenter.GuanZhuPresenter;
 import com.bw.com.onetimedemo.view.MyGuanZhuView;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 import java.util.List;
 
-public class MyGuanZhuActivity extends AppCompatActivity implements MyGuanZhuView {
+import base.BaseActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MyGuanZhuActivity extends BaseActivity implements MyGuanZhuView,View.OnClickListener {
+
+    @BindView(R.id.t_fh)
+    TextView tFh;
+    @BindView(R.id.t_gz)
+    TextView tGz;
+    @BindView(R.id.t_rmgz)
+    TextView tRmgz;
+    @BindView(R.id.edit_text)
+    EditText editText;
+    @BindView(R.id.x_v_gz)
+    XRecyclerView xVGz;
     private GuanZhuPresenter presenter;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_guan_zhu);
+    public int bindLayout() {
+        return R.layout.activity_my_guan_zhu;
+    }
+
+    @Override
+    public void initData() {
         presenter = new GuanZhuPresenter(this);
         presenter.attachView(this);
         //page=2&source=android&appVersion=101&token=0
-        presenter.getGoodsList("2","android","101","0");
+        presenter.getGoodsList("2", "android", "101", "0");
+        tFh.setOnClickListener(this);
+        tGz.setOnClickListener(this);
+        tRmgz.setOnClickListener(this);
+        editText.setOnClickListener(this);
     }
 
     @Override
@@ -37,5 +62,39 @@ public class MyGuanZhuActivity extends AppCompatActivity implements MyGuanZhuVie
     protected void onDestroy() {
         super.onDestroy();
         presenter.detachView(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.t_fh:
+                //返回
+               tFh.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       finish();
+                   }
+               });
+                break;
+            case R.id.t_gz:
+                //我的关注
+                break;
+            case R.id.t_rmgz:
+                //热门关注
+                tRmgz.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        tGz.setText("热门关注");
+                        editText.setVisibility(View.VISIBLE);
+
+                    }
+                });
+                break;
+            case R.id.edit_text:
+                //输入框 搜索
+                break;
+        }
+
     }
 }
